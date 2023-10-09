09b651b93eedb898091c224043318d7df3568ddb
Albert Caballero Coll <alcaball@cbr8s5.42barcelona.com>
2023-10-09 13:37:24 +0200

    fhola

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 15:30:56 by alcaball          #+#    #+#             */
-/*   Updated: 2023/10/06 18:20:25 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/10/09 13:36:16 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,27 @@ t_info	win_size(int fd)
 {
 	t_info	window;
 	int		i;
+	int		j;
 	char	*line;
 
 	i = 0;
+	j = 0;
 	line = get_next_line(fd);
 	while (line[i] != 0 && line[i] != 10)
+	{
+		if (line[i] != 32 && line[i + 1] == 32)
+			j++;
 		i++;
-	window.width = i;
+	}
+	window.width = j + 1;
 	i = 1;
 	while (get_next_line(fd) != NULL)
 		i++;
 	window.height = i;
+	close(fd);
 	return (window);
 }
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -37,23 +45,44 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	square(t_data *img, int x, int y)
+void	square(t_data *img, int w, int h, int xst, int yst)
 {
 	int	ix;
 	int	iy;
 
-	iy = 10;
-	while (iy < y)
+	iy = yst;
+	while (iy < h+yst)
 	{
-		ix = 10;
-		while (ix < x)
+		ix = xst;
+		while (ix < w + xst)
 		{
-			my_mlx_pixel_put(img, ix, iy, 0x00FF0000);
+			if (ix == xst || ix == w + xst - 1 || iy == yst || iy == h + yst - 1)
+				my_mlx_pixel_put(img, ix, iy, 0x00FF0000);
 			ix++;
 		}
 		iy++;
 	}
 
+}
+
+void	grid(t_data *img, int w, int h)
+{
+	int	vert;
+	int	horiz;
+	int	xsize = 20;
+	int	ysize = 20;
+
+	vert = 0;
+	while (vert < h)
+	{
+		horiz = 0;
+		while (horiz < w)
+		{
+			square(img, xsize, ysize, 100 + xsize * horiz, 100 + ysize * vert);
+			horiz++;
+		}
+		vert++;
+	}
 }
 
 void	circle(t_data *img, int h)
@@ -69,7 +98,7 @@ void	circle(t_data *img, int h)
 		ix = 0;
 		while (ix < 400)
 		{
-			if (pow((ix-200), 2)+pow((iy-200), 2) == pow(h/2, 2))
+			if (pow((ix - 200), 2) + pow((iy - 200), 2) == pow(h / 2, 2))
 			{
 				my_mlx_pixel_put(img, ix, iy, 0x0000FF00);
 				ft_printf("%i, %i ||", ix, iy);
@@ -83,6 +112,50 @@ void	circle(t_data *img, int h)
 
 }
 
+void	ft_free(char *gnline, char **splited)
+{
+	int	i;
+
+	i = 0;
+	if (gnline)
+		free(gnline);
+	while (splited[i] != NULL)
+	{
+		free(splited[i]);
+		i++;
+	}
+	free (splited);
+}
+
+int	*read_map(char *argv, t_info mapdim)
+{
+	int		*values;
+	int		map_fd;
+	char	*line;
+	char	**splited;
+	int		i;
+	int		j;
+
+	map_fd = open(argv, R_OK);
+	if (map_fd < 0)
+		return (NULL);
+	line = get_next_line(map_fd);
+	i = 0;
+	values = malloc (sizeof(int) * mapdim.width * mapdim.height);
+	while (line != NULL)
+	{
+		j = 0;
+		splited = ft_split(line, ' ');
+		while (splited[j] != NULL)
+			values[i++] = ft_atoi(splited[j++]);
+		ft_free(line, splited);
+		line = get_next_line(map_fd);
+	}
+	free (line);
+	close (map_fd);
+	return (values);
+}
+
 int	main(int argc, char **argv)
 {
 	int		map_fd;
@@ -90,6 +163,7 @@ int	main(int argc, char **argv)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	int		*map;
 
 	if (argc < 2)
 		return (0);
@@ -98,12 +172,13 @@ int	main(int argc, char **argv)
 		return (0);
 	mlx = mlx_init();
 	window = win_size(map_fd);
-	mlx_win = mlx_new_window(mlx, 400, 400, "FDF");
-	img.img = mlx_new_image(mlx, 400, 400);
+	map = read_map(argv[1], window);
+	mlx_win = mlx_new_window(mlx, 700, 700, "FDF");
+	img.img = mlx_new_image(mlx, 700, 700);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	//square(&img, 200, 200);
-	circle(&img, 200);
+	grid(&img, window.width, window.height);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+	close (map_fd);
 	return (0);
 }
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 15:31:08 by alcaball          #+#    #+#             */
-/*   Updated: 2023/10/06 16:37:10 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/10/09 13:36:43 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,14 @@
 # include <fcntl.h>
 # include "minilibx/mlx.h"
 # include <math.h>
+# include <stdlib.h>
 
 size_t	ft_strlen(const char *str);
 int		ft_printf(const char *str, ...);
 char	*get_next_line(int fd);
+int		ft_atoi(const char *str);
+char	**ft_split(char const *s, char c);
+
 
 typedef struct s_information
 {
